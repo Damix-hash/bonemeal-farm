69b8e8a446cd3435ec2ed193ba81d877a94f521a
Damix <158813145+Damix-hash@users.noreply.github.com>
2025-08-15 12:39:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,28 @@
 const mineflayer = require('mineflayer');
 
-const PASSWORD = process.env.MC_PASSWORD || '';
-
+const PASSWORD = process.env.MC_PASSWORD;
+const possible_anwsers = {
+  "Unscramble: TINREHHE": "thermite",
+  "What block can't be destroyed by TNT?": "obsidian",
+  //"": "enderman",
+  //"": "kreit", //?
+  "Unscramble: TNERD": "render",
+  "1, 3, 5, 7, ?": "9",
+  "Say: Redstone rules": "redstone rules",
+  "I'm red, I'm dust, I power redstone. What am I?": "redstone",
+  "2, 4, 8, ?": "16",
+  //"": "build",
+  "TNT can't break what?": "bedrock", // It's the strongest block in the game
+  "1, 3, 5, ?": "7",
+  "Unscramble: NIT": "tin",
+  "Unscramble: LAV": "val",
+  "I can be red or blue, but I'm not a dye. I can be found in the Overworld, but I'm not a tree. What am I?": "wool",
+  "Unscramble: LAVAR": "lavar", // misspell LMAO
+  //"": "rite",
+  "Unscramble: KITS": "kits",
+  "2, 4, 8, 16, 32, ?": "64",
+  "Say: Anarchy builds": "anarchy builds"
+}
 function createBot() {
   const bot = mineflayer.createBot({
     host: 'eu.6b6t.org',
@@ -21,29 +42,26 @@ function createBot() {
   });
 
   bot.on('messagestr', (message) => {
-    function return_user(msg) {
-        let no_rank_message = '';
-        let get_username = '';
-
-        if (msg.startsWith('[')) {
-            no_rank_message = msg.split(']')[1];
-            get_username = no_rank_message.split('»')[0];
-        } else if (msg.includes('whispers')) {
-            get_username = msg.split('whispers')[0];
-        } else {
-            get_username = msg.split('»')[0];
+    function getRightAnwser(message) {
+      if (message.startsWith('[Prime] royalburner »')) {
+        console.log(message)
+        for (key in possible_anwsers) {
+          if (message.includes(key)) {
+            setTimeout(() => {
+              bot.chat(possible_anwsers[key])
+            }, Math.floor((Math.random() * 2000) + 1000))
+          }
         }
+      }
+    }
+    getRightAnwser(message)
 
-        return get_username?.trim() || '';
+    if (message.includes("royalburner wants to teleport to you.")) {
+      bot.chat("/tpy royalburner")
     }
 
-    if ((return_user(message) === 'Damix2131' || return_user(message) === 'MioAutoCrystal') && message.includes("come catstar")) {
-      bot.chat(`/tpa ${return_user(message)}`)
-    }
-
-    if ((return_user(message) === 'Damix2131' || return_user(message) === 'MioAutoCrystal') && message.includes("catstar say")) {
-      
-      bot.chat(message.split('catstar say ')[1])
+    if (message.includes("[Elite] Damix2131 » come catstar")) {
+      bot.chat("/tpa Damix2131")
     }
   })
 
